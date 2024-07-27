2d91fa084503d445a81c62ab63205b1109f5c284
Sara Cananda <112910022+saracananda@users.noreply.github.com>
2024-07-27 10:35:34 -0300

    Redefinição de senha
    - criação e configuração de urls e templates
    - mudanças no settings

--- usuario/urls.py
+++ usuario/urls.py
@@ -4,7 +4,14 @@ from . import views
 # app_name = "conta"
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'), 
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('register/', views.register, name = 'registrar-usuario'),  
-    path('listar/', views.listarUsuarios, name = 'listar-usuarios')
-]
+    path('listar/', views.listarUsuarios, name = 'listar-usuarios'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name = 'password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name = 'password_reset_done'), 
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name = 'password_reset_confirm'), #recebe a informação do email para receber token e liberar alteração
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name = 'password_reset_complete'), 
+    # path('', include('django.contrib.auth.urls'))
+
+] 
