4c43fe6357b8362b5bf1df85821db9552ec0f281
Gustavo Santos <estudosgusta32@gmail.com>
2024-07-29 01:16:25 -0300

    Novas funcionalidades
    O sistema deve permitir que os administradores visualizem o perfil de cada usuário (detalharUsuario lá na listagem, é só criar uma url e uma view, pegar o id e puxar com ele os dados do cara, daí mostrar)
    O sistema deverá permitir que o administrador acesse o histórico de solicitações de um usuário através do seu perfil (ISSO AQUI É DENTRO DO DETALHAR USUÁRIO, FAZ SE DER)
    O sistema deve permitir que o solicitante acompanhe a sua solicitação de reserva (listar solicitações que tem o id dele  e botar um botãozinho com o andamento e outro com “cancelar solicitação”
    O sistema deve exibir, ao fim da solicitação, uma mensagem de confirmação de envio
    O sistema deverá armazenar o histórico de solicitações realizadas

--- reserva/urls.py
+++ reserva/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('reservar/', views.criar_reserva, name='registrar_reserva'),
     path('reservas/', views.listar_reservas, name='listar_reservas'),
+    path('historico/', views.historicoSolicitacao, name='historico_solicitacoes'),
+    path('reservar/enviada/<int:reserva_id>', views.solicitacaoEnviada, name='solicitacao_enviada'),
 ]
--- reserva/views.py
+++ reserva/views.py
@@ -3,10 +3,13 @@ from django.utils.dateparse import parse_datetime
 from django.utils import timezone
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.db.models import Q
 from .forms import ReservaForm
 from .models import Reserva, ReservaEspaco, ReservaEquipamento
 from recursos.models import Equipamento, Espaco
 from usuario.models import Usuario
+from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
 
 @login_required
 def criar_reserva(request):
@@ -23,15 +26,15 @@ def criar_reserva(request):
         # Verificação de datas
         if data_horario_inicial_dt < timezone.now():
             messages.error(request, 'A data e hora inicial não pode ser no passado.')
-            return render(request, 'partials/reserva/testandoReserva.html', {'form': ReservaForm()})
+            return render(request, 'partials/reserva/formReserva.html', {'form': ReservaForm()})
 
         if data_horario_inicial_dt >= data_horario_final_dt:
             messages.error(request, 'A data e hora final deve ser posterior à data e hora inicial.')
-            return render(request, 'partials/reserva/testandoReserva.html', {'form': ReservaForm()})
+            return render(request, 'partials/reserva/formReserva.html', {'form': ReservaForm()})
 
         if 'buscar' in request.POST:
             espacos_disponiveis, equipamentos_disponiveis = Reserva.verificar_disponibilidade(data_horario_inicial_dt, data_horario_final_dt)
-            return render(request, 'partials/reserva/testandoReserva.html', {
+            return render(request, 'partials/reserva/formReserva.html', {
                 'espacos_disponiveis': espacos_disponiveis,
                 'equipamentos_disponiveis': equipamentos_disponiveis,
                 'dataHorarioInicial': dataHorarioInicial,
@@ -45,7 +48,7 @@ def criar_reserva(request):
             if not espacos_selecionados and not equipamentos_selecionados or not justificativa:
                 messages.error(request, 'Lembre-se que você deve selecionar pelo menos um espaço ou equipamento e preencher a justificativa.')
                 espacos_disponiveis, equipamentos_disponiveis = Reserva.verificar_disponibilidade(data_horario_inicial_dt, data_horario_final_dt)
-                return render(request, 'partials/reserva/testandoReserva.html', {
+                return render(request, 'partials/reserva/formReserva.html', {
                     'espacos_disponiveis': espacos_disponiveis,
                     'equipamentos_disponiveis': equipamentos_disponiveis,
                     'dataHorarioInicial': dataHorarioInicial,
@@ -64,12 +67,54 @@ def criar_reserva(request):
             for equipamento_id in equipamentos_selecionados:
                 equipamento = Equipamento.objects.get(pk=equipamento_id)
                 ReservaEquipamento.objects.create(reserva=reserva, recurso=equipamento)
-            return redirect('listar_reservas')
+            return redirect('solicitacao_enviada', reserva_id=reserva.id)
     else:
         form = ReservaForm()
-    return render(request, 'partials/reserva/testandoReserva.html', {'form': form})
+    return render(request, 'partials/reserva/formReserva.html', {'form': form})
+
+@login_required
+def solicitacaoEnviada(request, reserva_id):
+    reserva = get_object_or_404(Reserva, pk=reserva_id)
+
+    if request.user.groups.filter(name="Administrador Master").exists():
+        tipo_adm = "Adm Master"
+    elif request.user.groups.filter(name="Administrador de Setor").exists():
+        tipo_adm = "Adm Setor"
+    else:
+        tipo_adm = "Solicitante"
+    return render(request, 'partials/reserva/formReserva_done.html', {
+        'dataHorarioInicial': reserva.dataHorarioInicial,
+        'dataHorarioFinal': reserva.dataHorarioFinal,
+        'codigo_solicitacao': reserva_id,
+        'tipo_adm': tipo_adm,
+    })
 
 @login_required
 def listar_reservas(request):
     reservas = Reserva.objects.all()
     return render(request, 'partials/reserva/lista_reserva.html', {'reservas': reservas})
+
+
+@login_required
+def historicoSolicitacao(request):
+    search_query = request.GET.get('searchbar', '') 
+    if search_query:
+        reservas_list = Reserva.objects.filter(
+            Q(usuario__username__icontains=search_query)
+        )
+    else:
+        reservas_list = Reserva.objects.all()
+
+    total_reservas = reservas_list.count()  # Total de reservas
+
+    # Paginação
+    paginator = Paginator(reservas_list, 8)  # Mostra 10 reservas por página
+    page_number = request.GET.get('page')
+    reservas = paginator.get_page(page_number)
+
+    return render(request, "partials/reserva/historico/historicoSolicitacoes.html", {
+        'reservas': reservas,
+        'total_reservas': total_reservas,
+        'search_query': search_query,
+        'page_obj': reservas
+    })
